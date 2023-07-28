efe8a78e19e8f8637069e5f10da2b3031683204f
Persiwal4 <mateuszbarwicki2@gmail.com>
2023-07-28 17:44:11 +0200

    Add publicPath and asset parser

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,22 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/' //this is not needed since webpack5 but can be used in some custom cases
   },
   mode: 'none',
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
-        type: 'asset/resource'
+        //with type:asset webpack will automatically decided if he should use resource or inline
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            //if the img size is smaller than the 3 kilobytes it will use inline, otherwise resource
+            maxSize: 3 * 1024 // 3 kilobytes
+          }
+        }
       }
     ]
   }
