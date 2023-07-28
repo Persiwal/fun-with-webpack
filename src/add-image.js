6c439035c61edc62f1d5678cc3b512ae4efbbf57
Persiwal4 <mateuszbarwicki2@gmail.com>
2023-07-28 18:05:38 +0200

    Use imported altText in add-image function

--- src/add-image.js
+++ src/add-image.js
@@ -1,8 +1,9 @@
 import Image from './image.png';
+import altText from './altText.txt';
 
 export default function addImage() {
   const img = document.createElement('img');
-  img.alt = 'image';
+  img.alt = altText;
   img.width = 300;
   img.src = Image;
   const body = document.querySelector('body');
