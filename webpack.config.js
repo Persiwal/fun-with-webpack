9936f9e19edca7a52c62bbf7c3c2bf7d721c9f9b
Persiwal4 <115703198+MateuszBarwicki@users.noreply.github.com>
2023-08-02 15:35:46 +0200

    Use latest JS features with babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,20 @@ module.exports = {
       //css-loader reads content of css file and return this content
       //style-loader take this conteont and injects it into the page using style tags
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            //compiles ecmasacript>5 to ecmascript 5
+            presets: ['@babel/env'],
+            //add if u want to use modern js feature whic is not supported by major browsers yet
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
     ]
   }
 };
